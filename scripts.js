7553db8cdcd2e12f97d48c6f67cfc13eed070b8b
Jeremy Lincoln <bossco28@gmail.com>
2017-04-28 16:56:24 -0400

    Added in click play/pause on the video

--- scripts.js
+++ scripts.js
@@ -1,13 +1,21 @@
 //get our elements
 const player = document.querySelector('.player');
-const video = player.querySelector('.video');
+const video = player.querySelector('video');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelector('[data-skip]');
 const ranges = player.querySelector('.player__slider');
 
-//build out function
+//Build out function
+function togglePlay(){
+    if(video.paused){
+        video.play();
+    } else {
+        video.pause();
+    }
+}
 
 
 //hook up the event listeners
+video.addEventListener('click', togglePlay);
